67ebad4a192c5193511ed31c15406b6bcdb32891
Jonathan Swale <jonathan.swale@gmail.com>
2014-07-23 22:44:39 +0200

    Update and rename KMHC.user.script to KMHC.user.js

--- src/KMHC.user.js
+++ src/KMHC.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name       		KFE
 // @namespace  		pharoz.net
-// @version    		0.0.1
+// @version    		0.0.2
 // @description  	Pharoz.net MH Connector
 // @match      		http://games.mountyhall.com/*
 // @require     	http://code.jquery.com/jquery-2.1.0.min.js
-// @downloadURL   https://github.com/jswale/KFE/raw/master/src/KMHC.user.script
+// @downloadURL   	https://github.com/jswale/KFE/raw/master/src/KMHC.user.js
+// @updateURL 		https://github.com/jswale/KFE/raw/master/src/KMHC.meta.js
 // @grant       	none
 // @copyright  		2014+, Miltown
 // ==/UserScript==
