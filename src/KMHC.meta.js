9867885d4be57466572480e1d419c5bf7edc0d37
Jonathan Swale <jonathan.swale@gmail.com>
2014-07-27 20:29:48 +0200

    Update KMHC.meta.js

--- src/KMHC.meta.js
+++ src/KMHC.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name       		KFE
 // @namespace  		pharoz.net
-// @version    		0.0.8
+// @version    		0.0.9
 // @description  	Pharoz.net MH Connector
 // @match      		http://games.mountyhall.com/*
 // @require     	http://code.jquery.com/jquery-2.1.0.min.js
-// @downloadURL     https://github.com/jswale/KFE/raw/master/src/KMHC.user.js
-// @updateURL       https://github.com/jswale/KFE/raw/master/src/KMHC.meta.js
+// @downloadURL   https://github.com/jswale/KFE/raw/master/src/KMHC.user.js
+// @updateURL     https://github.com/jswale/KFE/raw/master/src/KMHC.meta.js
 // @grant       	none
 // @copyright  		2014+, Miltown
 // ==/UserScript==
