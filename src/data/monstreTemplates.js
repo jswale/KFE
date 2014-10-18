00da3a719960e57cae778197ed7f57be98e7271b
Jonathan Swale <jonathan.swale@gmail.com>
2014-10-18 23:12:31 +0200

    Create monstreTemplates.js

---
+++ src/data/monstreTemplates.js
@@ -0,0 +1,565 @@
+var DB_monsterTemplate = {
+	"Alchimiste" : {
+		"name" : "Alchimiste",
+		"spe" : "Ramasse des objets Utilise des potions",
+		"familly" : "Humanoïde"
+	},
+	"Agressif" : {
+		"name" : "Agressif",
+		"level" : 1,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Humanoïde"
+	},
+	"Alpha" : {
+		"name" : "Alpha",
+		"level" : 11,
+		"nbAtt" : "x2",
+		"attDist" : true,
+		"hp" : "x3",
+		"att" : "x3",
+		"esq" : "x3",
+		"deg" : "x3",
+		"reg" : "x3",
+		"arm" : "x3",
+		"rm" : "x3",
+		"mm" : "x3",
+		"vue" : "normal",
+		"power" : "x3",
+		"spe" : "DLA plus Lente",
+		"familly" : "Insecte"
+	},
+	"Archaique" : {
+		"name" : "Archaique",
+		"level" : -1,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Mort-vivant"
+	},
+	"Archiatre" : {
+		"name" : "Archiatre",
+		"level" : 2,
+		"nbAtt" : -1,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Soin des monstres",
+		"familly" : "Demon"
+	},
+	"Attentionné" : {
+		"name" : "Attentionné",
+		"level" : 2,
+		"nbAtt" : 1,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Soin des monstres",
+		"familly" : "Animal"
+	},
+	"Barbare" : {
+		"name" : "Barbare",
+		"level" : 1,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Humanoïde"
+	},
+	"Berserker" : {
+		"name" : "Berserker",
+		"level" : 3,
+		"nbAtt" : "x2",
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Humanoïde"
+	},
+	"Champion" : {
+		"name" : "Champion",
+		"level" : 4,
+		"attDist" : false,
+		"vlc" : false,
+		"esq" : 2,
+		"deg" : 2,
+		"reg" : 1,
+		"arm" : 2,
+		"familly" : "Humanoïde"
+	},
+	"Cogneur" : {
+		"name" : "Cogneur",
+		"level" : 2,
+		"attMag" : "Jamais",
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Amnesie",
+		"familly" : "Tous"
+	},
+	"Colossal" : {
+		"name" : "Colossal",
+		"level" : 7,
+		"attDist" : false,
+		"vlc" : false,
+		"esq" : 2,
+		"deg" : 2,
+		"reg" : 1,
+		"arm" : 2,
+		"familly" : "Monstre"
+	},
+	"Coriace" : {
+		"name" : "Coriace",
+		"level" : 1,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"esq" : -1,
+		"deg" : 2,
+		"reg" : 1,
+		"arm" : 4,
+		"vue" : 1,
+		"familly" : "Tous"
+	},
+	"Corrompu" : {
+		"name" : "Corrompu",
+		"level" : 1,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"att" : -2,
+		"esq" : -2,
+		"deg" : 1,
+		"vue" : -1,
+		"familly" : "Tous"
+	},
+	"Cracheur" : {
+		"name" : "Cracheur",
+		"level" : 2,
+		"attDist" : true,
+		"vlc" : false,
+		"spe" : "Attaque à distance",
+		"familly" : "Monstre"
+	},
+	"de Premier Cercle" : {
+		"name" : "de Premier Cercle",
+		"level" : -1,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Démon"
+	},
+	"de Second Cercle" : {
+		"name" : "de Second Cercle",
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Démon"
+	},
+	"de Troisième Cercle" : {
+		"name" : "de Troisième Cercle",
+		"level" : 2,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Démon"
+	},
+	"de Quatrième Cercle" : {
+		"name" : "de Quatrième Cercle",
+		"level" : 4,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Démon"
+	},
+	"de Cinquième Cercle" : {
+		"name" : "de Cinquième Cercle",
+		"level" : 5,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Démon"
+	},
+	"des Abysses" : {
+		"name" : "des Abysses",
+		"level" : 3,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Démon"
+	},
+	"Effrayé" : {
+		"name" : "Effrayé",
+		"level" : -1,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Fuyard",
+		"familly" : "Humanoïde"
+	},
+	"Enragé" : {
+		"name" : "Enragé",
+		"level" : 3,
+		"nbAtt" : "x2",
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Animal"
+	},
+	"Esculape" : {
+		"name" : "Esculape",
+		"level" : 2,
+		"nbAtt" : -1,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Soin des monstres",
+		"familly" : "Monstre"
+	},
+	"Ethéré" : {
+		"name" : "Ethéré",
+		"level" : 3,
+		"attMag" : true,
+		"attDist" : true,
+		"vlc" : false,
+		"spe" : "Camouflage",
+		"familly" : "Demon"
+	},
+	"Fanatique" : {
+		"name" : "Fanatique",
+		"level" : 2,
+		"nbAtt" : "x2",
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Ne fuit pas",
+		"familly" : "Humanoïde, Démon"
+	},
+	"Fou" : {
+		"name" : "Fou",
+		"level" : 1,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Humanoïde"
+	},
+	"Fouisseur" : {
+		"name" : "Fouisseur",
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "enfouit les trésors",
+		"familly" : "Animal, Insecte, Monstre"
+	},
+	"Frénétique" : {
+		"name" : "Frénétique",
+		"level" : 3,
+		"nbAtt" : "x2",
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Monstre"
+	},
+	"Frondeur" : {
+		"name" : "Frondeur",
+		"level" : 2,
+		"attDist" : true,
+		"vlc" : false,
+		"hp" : -40,
+		"att" : 2,
+		"esq" : 2,
+		"familly" : "Humanoïde"
+	},
+	"Fustigateur" : {
+		"name" : "Fustigateur",
+		"level" : 2,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Flagellation mentale",
+		"familly" : "Monstre"
+	},
+	"Gardien" : {
+		"name" : "Gardien",
+		"level" : 20,
+		"attDist" : false,
+		"familly" : "Tous"
+	},
+	"Gargantuesque" : {
+		"name" : "Gargantuesque",
+		"level" : 3,
+		"attDist" : false,
+		"vlc" : false,
+		"esq" : -40,
+		"deg" : 2,
+		"reg" : 2,
+		"familly" : "Monstre"
+	},
+	"Gigantesque" : {
+		"name" : "Gigantesque",
+		"level" : 1,
+		"attDist" : true,
+		"vlc" : false,
+		"familly" : "Animal, Insecte, Monstre"
+	},
+	"Grand Frondeur" : {
+		"name" : "Grand Frondeur",
+		"level" : 4,
+		"attDist" : true,
+		"vlc" : false,
+		"esq" : -40,
+		"deg" : 2,
+		"reg" : 2,
+		"familly" : "Humanoïde"
+	},
+	"Gros" : {
+		"name" : "Gros",
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Tous"
+	},
+	"Guérisseur" : {
+		"name" : "Guérisseur",
+		"level" : 2,
+		"nbAtt" : -1,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Soin des monstres",
+		"familly" : "Humanoide"
+	},
+	"Guerrier" : {
+		"name" : "Guerrier",
+		"level" : 1,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Humanoïde"
+	},
+	"Héros" : {
+		"name" : "Héros",
+		"level" : 5,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "DLA plus courte",
+		"familly" : "Humanoïde"
+	},
+	"Homochrome" : {
+		"name" : "Homochrome",
+		"attMag" : false,
+		"attDist" : true,
+		"vlc" : false,
+		"spe" : "Camouflage",
+		"familly" : "Animal, Insecte"
+	},
+	"Homomorphe" : {
+		"name" : "Homomorphe",
+		"attDist" : true,
+		"vlc" : false,
+		"spe" : "Camouflage",
+		"familly" : "Monstre"
+	},
+	"Implacable" : {
+		"name" : "Implacable",
+		"level" : 3,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Mort-vivant"
+	},
+	"Invocateur" : {
+		"name" : "Invocateur",
+		"level" : 3,
+		"attMag" : true,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Appel sous certaines conditions",
+		"familly" : "Demon"
+	},
+	"Maitre" : {
+		"name" : "Maitre",
+		"level" : 8,
+		"nbAtt" : 1, //"+1 ou x2 ?",
+		"attDist" : false,
+		"vlc" : true,
+		"familly" : "Mort-vivant"
+	},
+	"Lobotomisateur" : {
+		"name" : "Lobotomisateur",
+		"level" : 2,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Flagellation mentale mais uniquement sur compétence",
+		"familly" : "Insecte"
+	},
+	"Malade" : {
+		"name" : "Malade",
+		"level" : -1,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Maladie + Perte de Pouvoir",
+		"familly" : "Tous"
+	},
+	"Médicastre" : {
+		"name" : "Médicastre",
+		"level" : 2,
+		"nbAtt" : -1,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Soin des monstres",
+		"familly" : "Mort-vivant"
+	},
+	"Mentat" : {
+		"name" : "Mentat",
+		"level" : 2,
+		"attMag" : true,
+		"attDist" : true,
+		"vlc" : false,
+		"familly" : "Mort-vivant"
+	},
+	"Morticole" : {
+		"name" : "Morticole",
+		"level" : 2,
+		"nbAtt" : -1,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Soin des monstres",
+		"familly" : "Insecte"
+	},
+	"Mutant" : {
+		"name" : "Mutant",
+		"level" : 2,
+		"attMag" : true,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Humanoïde"
+	},
+	"Nécromant" : {
+		"name" : "Nécromant",
+		"level" : 5,
+		"attMag" : true,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Appel",
+		"familly" : "Mort-vivant"
+	},
+	"Ouvrier" : {
+		"name" : "Ouvrier",
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Insecte"
+	},
+	"Paysan" : {
+		"name" : "Paysan",
+		"level" : -1,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Humanoïde"
+	},
+	"Petit" : {
+		"name" : "Petit",
+		"level" : -1,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Tous"
+	},
+	"Planque" : {
+		"name" : "Planque",
+		"attDist" : true,
+		"vlc" : false,
+		"spe" : "Camouflage",
+		"familly" : "Humanoide"
+	},
+	"Prince" : {
+		"name" : "Prince",
+		"level" : 8,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "DLA très courte",
+		"familly" : "Démon"
+	},
+	"Psychophage" : {
+		"name" : "Psychophage",
+		"level" : 2,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Flagellation mentale mais uniquement sur sortilège",
+		"familly" : "Mort-vivant"
+	},
+	"Reine" : {
+		"name" : "Reine",
+		"level" : 11,
+		"nbAtt" : 1, //"+1 ou x2 ?",
+		"attDist" : true,
+		"vlc" : true,
+		"familly" : "Insecte"
+	},
+	"Ronfleur" : {
+		"name" : "Ronfleur",
+		"level" : 2,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Ronflage",
+		"familly" : "Tous"
+	},
+	"Scout" : {
+		"name" : "Scout",
+		"level" : 1,
+		"attMag" : false,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Humanoïde"
+	},
+	"Shaman" : {
+		"name" : "Shaman",
+		"attMag" : true,
+		"attDist" : true,
+		"vlc" : false,
+		"spe" : "si pas de pouvoir, donne un pouvoir parmi : acide, attaque affaiblissante, attaque aveuglante, attaque életrique, attaque paralysante, attaque gluante, aura de feu, coup perforant ou venin",
+		"familly" : "Humanoïde"
+	},
+	"Soldat" : {
+		"name" : "Soldat",
+		"level" : 3,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Insecte"
+	},
+	"Sorcier" : {
+		"name" : "Sorcier",
+		"attDist" : true,
+		"vlc" : true,
+		"spe" : "si pas de pouvoir, donne un pouvoir parmi bénédiction",
+		"familly" : "Humanoïde"
+	},
+	"Spectral" : {
+		"name" : "Spectral",
+		"attDist" : true,
+		"vlc" : false,
+		"spe" : "Camouflage",
+		"familly" : "Mort-vivant"
+	},
+	"Strident" : {
+		"name" : "Strident",
+		"level" : 3,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "Strident / auto",
+		"familly" : "Insecte"
+	},
+	"Traqueur" : {
+		"name" : "Traqueur",
+		"level" : 1,
+		"attDist" : false,
+		"vlc" : true,
+		"familly" : "Monstre"
+	},
+	"Voleur" : {
+		"name" : "Voleur",
+		"level" : 2,
+		"attDist" : false,
+		"vlc" : false,
+		"spe" : "vol d'objet",
+		"familly" : "Humanoïde"
+	},
+	"Vorace" : {
+		"name" : "Vorace",
+		"level" : 1,
+		"attDist" : false,
+		"vlc" : false,
+		"familly" : "Animal"
+	}
+};
