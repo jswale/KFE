c1b4908e49dba2e1ffa34261f9482936654511f0
Jonathan Swale <jonathan.swale@gmail.com>
2014-10-20 12:54:05 +0200

    Update monstreAges.js

--- src/data/monstreAges.js
+++ src/data/monstreAges.js
@@ -1,146 +1,221 @@
 var DB_monsterAges = {
-	"Bébé" : {
-		"level" : 0
+	"Animal" : {
+		"Bébé" : {
+			"level" : 0
+		},
+		"Enfançon" : {
+			"level" : 1
+		},
+		"Jeune" : {
+			"level" : 2
+		},
+		"Adulte" : {
+			"level" : 3
+		},
+		"Mature" : {
+			"level" : 4
+		},
+		"Chef de Harde" : {
+			"level" : 5
+		},
+		"Ancien" : {
+			"level" : 6
+		},
+		"Ancienne" : {
+			"level" : 6
+		},
+		"Ancêtre" : {
+			"level" : 7
+		}
 	},
-	"Initial" : {
-		"level" : 0
+	"Démon" : {
+		"Initial" : {
+			"level" : 0
+		},
+		"Initiale" : {
+			"level" : 0
+		},
+		"Novice" : {
+			"level" : 1
+		},
+		"Mineur" : {
+			"level" : 2
+		},
+		"Mineure" : {
+			"level" : 2
+		},
+		"Favori" : {
+			"level" : 3
+		},
+		"Favorite" : {
+			"level" : 3
+		},
+		"Majeur" : {
+			"level" : 4
+		},
+		"Majeure" : {
+			"level" : 4
+		},
+		"Supérieur" : {
+			"level" : 5
+		},
+		"Supérieure" : {
+			"level" : 5
+		},
+		"Suprème" : {
+			"level" : 6
+		},
+		"Ultime" : {
+			"level" : 7
+		}
 	},
-	"Initiale" : {
-		"level" : 0
+	"Humanoïde" : {
+		"Nouveau" : {
+			"level" : 0
+		},
+		"Nouvelle" : {
+			"level" : 0
+		},
+		"Jeune" : {
+			"level" : 1
+		},
+		"Adulte" : {
+			"level" : 2
+		},
+		"Vétéran" : {
+			"level" : 3
+		},
+		"Briscard" : {
+			"level" : 4
+		},
+		"Briscarde" : {
+			"level" : 4
+		},
+		"Doyen" : {
+			"level" : 5
+		},
+		"Doyenne" : {
+			"level" : 5
+		},
+		"Légendaire" : {
+			"level" : 6
+		},
+		"Mythique" : {
+			"level" : 7
+		}
 	},
-	"Nouveau" : {
-		"level" : 0
+	"Insecte" : {
+		"Larve" : {
+			"level" : 0
+		},
+		"Immature" : {
+			"level" : 1
+		},
+		"Juvénile" : {
+			"level" : 2
+		},
+		"Imago" : {
+			"level" : 3
+		},
+		"Développé" : {
+			"level" : 4
+		},
+		"Développée" : {
+			"level" : 4
+		},
+		"Mûr" : {
+			"level" : 5
+		},
+		"Mûre" : {
+			"level" : 5
+		},
+		"Accompli" : {
+			"level" : 6
+		},
+		"Accomplie" : {
+			"level" : 6
+		},
+		"Achevé" : {
+			"level" : 7
+		},
+		"Achevée" : {
+			"level" : 7
+		}
 	},
-	"Nouvelle" : {
-		"level" : 0
+	"Monstre" : {
+		"Nouveau" : {
+			"level" : 0
+		},
+		"Nouvelle" : {
+			"level" : 0
+		},
+		"Jeune" : {
+			"level" : 1
+		},
+		"Adulte" : {
+			"level" : 2
+		},
+		"Vétéran" : {
+			"level" : 3
+		},
+		"Briscard" : {
+			"level" : 4
+		},
+		"Briscarde" : {
+			"level" : 4
+		},
+		"Doyen" : {
+			"level" : 5
+		},
+		"Doyenne" : {
+			"level" : 5
+		},
+		"Légendaire" : {
+			"level" : 6
+		},
+		"Mythique" : {
+			"level" : 7
+		}
 	},
-	"Larve" : {
-		"level" : 0
-	},
-	"Naissant" : {
-		"level" : 0
-	},
-	"Naissante" : {
-		"level" : 0
-	},
-	"Enfançon" : {
-		"level" : 1
-	},
-	"Novice" : {
-		"level" : 1
-	},
-	"Jeune" : {
-		"level" : 2
-	},
-	"Immature" : {
-		"level" : 1
-	},
-	"Récent" : {
-		"level" : 1
-	},
-	"Récente" : {
-		"level" : 1
-	},
-	"Mineur" : {
-		"level" : 2
-	},
-	"Mineure" : {
-		"level" : 2
-	},
-	"Adulte" : {
-		"level" : 3
-	},
-	"Juvénile" : {
-		"level" : 2
-	},
-	"Ancien" : {
-		"level" : 6
-	},
-	"Ancienne" : {
-		"level" : 6
-	},
-	"Favori" : {
-		"level" : 3
-	},
-	"Favorite" : {
-		"level" : 3
-	},
-	"Vétéran" : {
-		"level" : 3
-	},
-	"Imago" : {
-		"level" : 3
-	},
-	"Vénérable" : {
-		"level" : 3
-	},
-	"Mature" : {
-		"level" : 4
-	},
-	"Majeur" : {
-		"level" : 4
-	},
-	"Briscard" : {
-		"level" : 4
-	},
-	"Développé" : {
-		"level" : 4
-	},
-	"Développée" : {
-		"level" : 4
-	},
-	"Séculaire" : {
-		"level" : 4
-	},
-	"Chef de Harde" : {
-		"level" : 5
-	},
-	"Supérieur" : {
-		"level" : 5
-	},
-	"Supérieure" : {
-		"level" : 5
-	},
-	"Doyen" : {
-		"level" : 5
-	},
-	"Doyenne" : {
-		"level" : 5
-	},
-	"Mûr" : {
-		"level" : 5
-	},
-	"Antique" : {
-		"level" : 5
-	},
-	"Suprême" : {
-		"level" : 6
-	},
-	"Légendaire" : {
-		"level" : 6
-	},
-	"Accompli" : {
-		"level" : 6
-	},
-	"Ancestral" : {
-		"level" : 6
-	},
-	"Ancêtre" : {
-		"level" : 7
-	},
-	"Ultime" : {
-		"level" : 7
-	},
-	"Mythique" : {
-		"level" : 7
-	},
-	"Achevé" : {
-		"level" : 7
-	},
-	"Achevée" : {
-		"level" : 7
-	},
-	"Antédiluvien" : {
-		"level" : 7
+	"Mort-Vivant" : {
+		"Naissant" : {
+			"level" : 0
+		},
+		"Naissante" : {
+			"level" : 0
+		},
+		"Récent" : {
+			"level" : 1
+		},
+		"Récente" : {
+			"level" : 1
+		},
+		"Ancien" : {
+			"level" : 2
+		},
+		"Ancienne" : {
+			"level" : 2
+		},
+		"Vénérable" : {
+			"level" : 3
+		},
+		"Séculaire" : {
+			"level" : 4
+		},
+		"Antique" : {
+			"level" : 5
+		},
+		"Ancestral" : {
+			"level" : 6
+		},
+		"Ancestrale" : {
+			"level" : 6
+		},
+		"Antédiluvien" : {
+			"level" : 7
+		},
+		"Antédiluvienne" : {
+			"level" : 7
+		}
 	}
 };
