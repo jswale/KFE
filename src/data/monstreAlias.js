c568607a28ea1e41278071ce374a1aeba44cfa55
Jonathan Swale <jonathan.swale@gmail.com>
2014-10-20 11:43:15 +0200

    Create monstreAlias.js

---
+++ src/data/monstreAlias.js
@@ -0,0 +1,46 @@
+var DB_monstreAlias = {
+  "Rat" : "Petit Rat Géant",
+  "Rat Gigantesque" : "Gros Rat Géant",
+  "Rat Gargantuesque" : "Rat Géant Gargantuesque",
+  
+  "Araignée" : "Petite Araignée Géante",
+  "Araignée Gigantesque" : "Grosse Araignée Géante",
+  "Araignée Gargantuesque" : "Araignée Géante Gargantuesque",
+  
+  "Chauve-Souris" : "Petite Chauve-Souris Géante",
+  "Chauve-Souris Gigantesque" : "Grosse Chauve-Souris Géante",
+  "Chauve-Souris Gargantuesque" : "Chauve-Souris Géante Gargantuesque",
+
+  "Lézard" : "Petit Lézard Géant",
+  "Lézard Gigantesque" : "Gros Lézard Géant",
+  "Lézard Gargantuesque" : "Lézard Géant Gargantuesque",
+  
+  "Scarabée" : "Petit Scarabée Géant",
+  "Scarabée Gigantesque" : "Gros Scarabée Géant",
+  "Scarabée Gargantuesque" : "Scarabée Géant Gargantuesque",
+  
+  "Fungus" : "Petit Fungus Géant",
+  "Fungus Gigantesque" : "Gros Fungus Géant",
+  "Fungus Gargantuesque" : "Fungus Géant Gargantuesque",
+  
+  "Amibe" : "Petite Amibe Géante",
+  "Amibe Gigantesque" : "Grosse Amibe Géante",
+  "Amibe Gargantuesque" : "Amibe Géante Gargantuesque",
+  
+  "Limace" : "Petite Limace Géante",
+  "Limace Gigantesque" : "Grosse Limace Géante",
+  "Limace Gargantuesque" : "Limace Géante Gargantuesque",
+  
+  "Ver Carnivore" : "Petit Ver Carnivore Géant",
+  "Ver Carnivore Gigantesque" : "Gros Ver Carnivore Géant",
+  "Ver Carnivore Gargantuesque" : "Ver Carnivore Géant Gargantuesque",
+  
+  "Scorpion" : "Petit Scorpion Géant",
+  "Scorpion Gigantesque" : "Gros Scorpion Géant",
+  "Scorpion Gargantuesque" : "Scorpion Géant Gargantuesque",
+  
+  "Mille-Pattes" : "Petit Mille-Pattes Géant",
+  "Mille-Pattes Gigantesque" : "Gros Mille-Pattes Géant",
+  "Mille-Pattes Gargantuesque" : "Mille-Pattes Géant Gargantuesque"
+  
+};
