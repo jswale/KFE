dc3adf477a49631ea7e712254b587a923a607f5f
d-faure <d-faure@users.noreply.github.com>
2015-06-15 13:36:24 +0200

    Update KMHC.meta.js

--- src/KMHC.meta.js
+++ src/KMHC.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          KFE
 // @namespace     pharoz.net
-// @version       0.1.4-11
+// @version       0.1.4-12
 // @description   Pharoz.net MH Connector
 // @match         http://games.mountyhall.com/*
 // @require       http://code.jquery.com/jquery-2.1.0.min.js
