6c009dda7bd5d8a9a3d00abf0ba39fd020959660
d-faure <d-faure@users.noreply.github.com>
2017-01-22 22:14:59 +0100

    Update KMHC.user.js
    removed graphic mapping

--- src/KMHC.user.js
+++ src/KMHC.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          KFE
 // @namespace     pharoz.net
-// @version       1.0.3-5
+// @version       1.0.3-6
 // @description   Pharoz.net MH Connector
 // @match         http://games.mountyhall.com/*
 // @require       http://code.jquery.com/jquery-2.1.4.min.js
@@ -238,169 +238,12 @@ var Utils = function() {
     }
 }();
 
-var Mapper = $.inherit({}, {
-    HOLES : [
-        // X, Y, size, radius, Ndepth
-        [-70.5, -7.5, 2, 1.5, -69],
-        [-66.5, -37.5, 2, 1.5, -69],
-        [-63.5, 8.5, 2, 1.5, -69],
-        [-59.5, -32.5, 2, 1.5, -69],
-        [-52, 57, 0.25, 0.8, -59],
-        [-50.5, -22.5, 2, 1.5, -69],
-        [-35.5, -51.5, 2, 1.5, -69],
-        [-34.5, 14.5, 2, 1.5, -69],
-        [-34.5, 64.5, 2, 1.5, -69],
-        [-11.5, 72.5, 2, 1.5, -69],
-        [5.5, -49.5, 2, 1.5, -69],
-        [5.5, 31.5, 2, 1.5, -69],
-        [10.5, 63.5, 2, 1.5, -69],
-        [12, -15, 0.25, 0.8, -59],
-        [21.5, 35.5, 2, 1.5, -69],
-        [30, -52, 0.25, 0.8, -59],
-        [46.5, 51.5, 2, 1.5, -69],
-        [48, -39, 0.25, 0.8, -59],
-        [55, 70, 0.25, 0.8, -69],
-        [56.5, 23.5, 75, 8.7, -99],
-        [64, 70, 0.25, 0.8, -59],
-        [74.5, 31.5, 2, 1.5, -69]
-    ],
-
-    LOCATIONS : {
-        "Véda": [-64, 47, -32, "rgba(138,43,226,0.75)"],
-        "Karlaaki": [84, 27, -50, "rgba(138,43,226,0.75)"]
-    },
-
-    xCtx : function(xMH, p) { return (xMH + 100) * p.zf + p.dx; },
-    yCtx : function(yMH, p) { return (100 - yMH) * p.zf + p.dy; },
-    xMh : function(xCTX, p) { return Math.round((xCTX - p.dx) / p.zf - 100); },
-    yMh : function(yCTX, p) { return Math.round(100 - (yCTX - p.dy) / p.zf); },
-
-    getMap : function(ref, p) {
-        var div = $("<div/>",{id: ref})
-        .css("padding", "20px");
-
-        var canvas = $("<canvas/>", {id: ref + "_canvas"})
-        .addClass("mh_tdtitre")
-        .appendTo(div)[0];
-
-        var ctx = canvas.getContext("2d");
-        var tip = $("<div/>",{id: ref + "_tip"})
-        .css("position", "absolute")
-        .css("max-width", "700px")
-        .css("border", "1px solid #CCC")
-        .css("background-color", "#FFF")
-        .css("border-radius", "5px")
-        .css("z-index", "999")
-        .css("display", "none")
-        .appendTo(div);
-
-        var tipH = $("<h2/>")
-        .css("background", "#333")
-        .css("border", "1px solid #111")
-        .css("padding", "5px 10px")
-        .css("font-size", "14px")
-        .css("color", "white")
-        .css("text-shadow", "0 -1px 0 rgba(0, 0, 0, 0.5)")
-        .css("letter-spacing", "0")
-        .css("border-radius", "5px 5px 0 0")
-        .css("font-weight", "normal")
-        .css("margin-bottom", "0px")
-        .css("margin-top", "0px")
-        .css("overflow", "hidden")
-        .text("-")
-        .appendTo(tip);
-
-        var tipD = $("<div/>")
-        .css("padding", "10px")
-        .appendTo(tip);
-
-        var map = {div: div, ctx: ctx, places: []};
-
-        canvas.width = 200 * p.zf + 2 * p.dx;
-        canvas.height = 200 * p.zf + 2 * p.dy;
-
-        // quadrants
-        ctx.beginPath();
-        ctx.moveTo(100 * p.zf + p.dx, p.dy);
-        ctx.lineTo(100 * p.zf + p.dx, 200 * p.zf + p.dy);
-        ctx.moveTo(p.dx, 100 * p.zf + p.dy);
-        ctx.lineTo(200 * p.zf + p.dx, 100 * p.zf + p.dy);
-        ctx.stroke();
-        ctx.strokeRect(p.dx, p.dy, p.zf * 200, p.zf * 200);
-
-        // holes
-        ctx.fillStyle = "rgb(200,0,0)";
-        $.each(this.HOLES, $.proxy(function (i, h) {
-            ctx.beginPath();
-            ctx.arc(this.xCtx(h[0], p), this.yCtx(h[1], p), h[3] * p.zf, 0, Math.PI * 2, true);
-            ctx.fill();
-        }, this));
-
-        // known locations
-        var lp = $.extend({lw: 1}, p);
-        $.each(this.LOCATIONS, $.proxy(function (name, loc) {
-            this.drawPos(map, loc[0], loc[1], loc[3], lp, name + " : X=" + loc[0] + " | Y=" + loc[1] + " | N=" + loc[2]);
-        }, this));
-
-        // tooltip
-        var canvasOffset = null;
-        $(canvas)
-        .on('mouseenter', function() { canvasOffset = $(canvas).offset(); })
-        .on('mouseout', function() { tip.css("display", "none"); canvasOffset = null; })
-        .on('mousemove', $.proxy(function(e) {
-            tip.css("left", (e.pageX + 20) + "px").css("top", (e.pageY + 10) + "px").css("display", "block");
-            if(!canvasOffset) return;
-            var pos = {
-                x: e.pageX - canvasOffset.left,
-                y: e.pageY - canvasOffset.top
-            };
-            pos.xMh = this.xMh(pos.x, p);
-            pos.yMh = this.yMh(pos.y, p);
-            pos.description = "";
-            tipH.text("[ X=" + pos.xMh + " | Y=" + pos.yMh + " ]");
-            $.each(this.HOLES, function (i, h) {
-                var dist = (pos.xMh - h[0]) * (pos.xMh - h[0]) + (pos.yMh - h[1]) * (pos.yMh - h[1]) - h[2];
-                if(dist <= 0) {
-                    pos.description = "Trou de Météorite : N=-1 => N=" + h[4];
-                    return false;
-                }
-            });
-
-            $.each(map.places, function (i, p) {
-                var dist = (pos.xMh - p.x) * (pos.xMh - p.x) + (pos.yMh - p.y) * (pos.yMh - p.y);
-                if(dist <= 0 && p.name) {
-                    pos.description = p.name;
-                    return false;
-                }
-            });
-
-            tipD.text(pos.description);
-        }, this));
-
-        return map;
-    },
-    drawPos : function(map, xMH, yMH, color, p, text) {
-        var x = this.xCtx(xMH, p), y = this.yCtx(yMH, p);
-        map.ctx.strokeStyle = color;
-        map.ctx.fillStyle = color;
-        map.ctx.lineWidth = p.lw || p.zf;
-        map.ctx.beginPath();
-        map.ctx.arc(x, y, p.zf, 0, Math.PI * 2, true);
-        map.ctx.fill();
-        map.ctx.beginPath();
-        map.ctx.arc(x, y, 3 * p.zf, 0, Math.PI * 2, true);
-        map.ctx.stroke();
-        map.places.push({x: xMH, y: yMH, name: text});
-    }
-});
-
 var Page = $.inherit({
 
     moduleName: null,
     logger : Logger,
     utils : Utils,
     display : Display,
-    mapper : Mapper,
 
     __constructor : function(moduleName) { // constructor
         this.moduleName = moduleName;
@@ -468,7 +311,7 @@ var Page = $.inherit({
                         var tmp = key.split(";");
                         if(tmp[0] == dataId) {
                             var o = $(selectSelector + " option[value='" + prefix + tmp[1] + suffix + "']");
-                            var cleared = o.text().split(" - ").slice(1).join(" - ");                            
+                            var cleared = o.text().split(" - ").slice(1).join(" - ");
                             if(data.tag.indexOf(cleared.split("(")[0]) == 0) {
                                 o.text(tmp[1] + " - " + data.tag);
                             } else {
@@ -3485,23 +3328,10 @@ var MH_Play_Play_vue = $.inherit(Page, {
 var MH_Lieux_Lieu_Description = $.inherit(Page, {
     init : function() {
         var p = {zf: 2.0, dx: 30, dy: 30};
-        var map = this.mapper.getMap("map_mh", p);
-
-        $("#footer1").before(map.div);
-
-        var ctn = null;
-        try {
-            ctn = window.parent.parent.parent.Sommaire.document;
-        } catch(e) {}
-        if (ctn) {
-            var tmp = /X\s*=\s*(-?\d+)\s*\|\s*Y\s*=\s*(-?\d+)/.exec($("div.infoMenu", ctn).first().text());
-            this.mapper.drawPos(map, parseInt(tmp[1]), parseInt(tmp[2]), "rgba(0,80,200,0.75)", p, "Vous êtes ici");
-        }
 
         var tmp1 = /Portail de Téléportation\s+\(Lieu n° (\d+)\)/.exec($("div.titre2").text());
         if(tmp1) {
             var tmp2 = /(mène en X = (-?\d+) \| Y = (-?\d+)[^\.]*)/.exec($("#description").text());
-            this.mapper.drawPos(map, parseInt(tmp2[2]), parseInt(tmp2[3]), "rgba(0,0,0,0.5)", p, "Destination TP");
 
             this.callAPIConnected({
                 api: "tag",
@@ -3530,7 +3360,6 @@ var MH_Play_Liste_Vente_ListeVente_view = $.inherit(Page, {
 var MH_Play_Play_e_follo = $.inherit(Page, {
     map : null,
     init : function() {
-        this.map = this.addMap();
         this.addActions();
         this.addEdition();
         this.displayTags();
@@ -3577,19 +3406,6 @@ var MH_Play_Play_e_follo = $.inherit(Page, {
         return null;
     },
 
-    addMap : function() {
-        var p = {zf: 2.0, dx: 30, dy: 30},
-            map = this.mapper.getMap("map_mh", p);
-        $("form[action='Play_action_Equipement.php']").after(map.div);
-
-        var myPos = this.getMyPosition();
-        if(null != myPos) {
-            this.mapper.drawPos(map, myPos.x, myPos.y, "rgba(0,80,200,0.75)", p, "Vous êtes ici");
-        }
-
-        return {p : p, map : map};
-    },
-
     addActions: function() {
 
       var myPos = this.getMyPosition();
@@ -3608,7 +3424,6 @@ var MH_Play_Play_e_follo = $.inherit(Page, {
               if(null != myPos) {
                   distance = " (à " + Math.max(Math.abs(goPos.x-myPos.x),Math.abs(goPos.y-myPos.y),Math.abs(goPos.n-myPos.n)) + " cases)";
               }
-              this.mapper.drawPos(this.map.map, goPos.x, goPos.y, "rgba(50,50,0,0.5)", this.map.p, fullname+distance);
           }
 
           $("<tr/>")
@@ -4078,11 +3893,11 @@ var MH_Play_Play_equipement = $.inherit(Page, { // DE
 
 var MH_Play_Actions_Play_a_Move = $.inherit(Page, { // DE
     init : function() {
-        
+
         Utils.addGlobalStyle([
             '.trapOn { color:#FFF; background:darkred; }'
-        ]);        
-        
+        ]);
+
         var x = parseInt($("[name='ai_XDepart']").val());
         var y = parseInt($("[name='ai_YDepart']").val());
         var n = parseInt($("[name='ai_NDepart']").val());
@@ -4110,7 +3925,7 @@ var MH_Play_Actions_Play_a_Move = $.inherit(Page, { // DE
                 $.each(json.traps || [], $.proxy(function(dataId, data){
                     traps.push(data.x+";"+data.y+";"+data.n);
                 },this));
-                
+
                 var fnCheckTrap = function() {
                     var nX = x + parseInt($("input[type='radio'][name='ai_DeplX']:checked").val());
                     var nY = y + parseInt($("input[type='radio'][name='ai_DeplY']:checked").val());
@@ -4125,14 +3940,10 @@ var MH_Play_Actions_Play_a_Move = $.inherit(Page, { // DE
                 $.each($("input[type='radio']"), function() {
                     $(this).click(fnCheckTrap);
                 });
-                
-                
-
             }, scope : this
- });
-        
+        });
     }
-});         
+});
 
 /*******************************************/
 /************* LAUNCH **********************/
